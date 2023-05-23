cb36d51126161f39d3fda4170214496ac197569e
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-05-23 08:24:30 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -5,4 +5,12 @@ var addItems=document.getElementsByClassName('title');
 addItems[0].style.fontWeight='bold';
 addItems[0].style.color='green';
 
+var listItems=document.getElementsByClassName('list-group-item');
+console.log(listItems);
+listItems[2].style.backgroundColor='green';
+
+for(var i=0;i<listItems.length;i++)
+{
+    listItems[i].style.fontWeight='bold';
+}
 
