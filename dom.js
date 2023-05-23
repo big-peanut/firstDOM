56a922207e5fef9195ef1e926ad8d3a66b40d642
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-05-23 19:43:56 +0530

    Update5 dom.js

--- dom.js
+++ dom.js
@@ -82,3 +82,73 @@ var x=document.querySelector('ul .list-group')
 var y=document.querySelector('ul li .list-group-item')
 x.insertBefore(node,y)
 
+var form=document.getElementById('addForm');
+var itemList=document.getElementById('items');
+var filter=document.getElementById('filter')
+//form submit event
+
+form.addEventListener('submit',addItem)
+//delete event
+itemList.addEventListener('click',removeItem)
+//filter event
+filter.addEventListener('keyup',filterItems)
+//add item
+
+function addItem(e)
+{
+    e.preventDefault();
+    //get input value
+    var newItem=document.getElementById('item').value;
+
+    //create new li elements
+    var li=document.createElement('li')
+    //add class
+    li.className='list-group-item'
+    // add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+    //create del button element
+    var del=document.createElement('button');
+    //add classes to del 
+    del.className='btn btn-danger btn-sm float-right delete';
+    del.appendChild(document.createTextNode('X'));
+    //append button to li
+    li.appendChild(del);
+    //append li to list
+    itemList.appendChild(li)
+}
+
+//remove item
+
+function removeItem(e)
+{
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm("are you sure?"))
+        {
+            var li=e.target.parentElement;
+            itemList.removeChild(li)
+        }
+    }
+}
+
+//filter items
+function filterItems(e)
+{
+    //convert text to lowercase
+    var text=e.target.value.toLowerCase();
+    //get li
+    var items=itemList.getElementsByTagName('li')
+    //convert html collection to array
+    Array.from(items).forEach(function(item)
+    {
+        var itemName=item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1)
+        {
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    })
+}
+
