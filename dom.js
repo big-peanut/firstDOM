af0ee0089caf6285394bed68a6cfdeacb21c2466
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-05-23 08:14:41 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,8 @@
+var headerTitle=document.getElementById("header-title");
+headerTitle.style.borderBottom="solid 3px #000";
+
+var addItems=document.getElementsByClassName('title');
+addItems[0].style.fontWeight='bold';
+addItems[0].style.color='green';
+
+
